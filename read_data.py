4342ab0d1dc6271451184f14ffbf61227fd4e6dc
fame <famephp@gmail.com>
2022-11-28 20:13:45 +0500

    read_data

--- read_data.py
+++ read_data.py
@@ -2,12 +2,12 @@ import json
 def read_data(file_path: str)->dict:
     """
     This function will read the json file and return the data as a dictionary.
-    
     Parameters:
         file_path (str): Path of the file to be read
     Returns:
         dict: Dictionary containing the data of the json file.
-    
     """
-    #open file
-    return
+    op = open(filename).read()
+    data = json.loads(op)
+    
+    return data
